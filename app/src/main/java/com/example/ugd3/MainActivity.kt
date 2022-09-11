5651a5c3d45ad97aa68c6d8eb22336a5dc39ade5
RGun44* <ricogunawan444@gmail.com*>
2022-09-11 15:48:28 +0700

    Menu Jadi

--- app/src/main/java/com/example/ugd3/MainActivity.kt
+++ app/src/main/java/com/example/ugd3/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.ugd3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.ugd3.fragment.DashboardFragment
+import com.example.ugd3.fragment.InfoFragment
+import com.example.ugd3.fragment.SettingsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private val dashboardFragment = DashboardFragment()
+    private val infoFragment = InfoFragment()
+    private val settingsFragment = SettingsFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        replaceFragment(dashboardFragment)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.ic_dashboard -> replaceFragment(dashboardFragment)
+                R.id.ic_info -> replaceFragment(infoFragment)
+                R.id.ic_settings -> replaceFragment(settingsFragment)
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+        if (fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
     }
 }
